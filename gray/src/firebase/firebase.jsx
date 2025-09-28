20e48ab159fa56913aa326a34940ac29a7e990da
kierre-yes <kier.reyesfr@gmail.com>
2025-09-28 20:34:28 +0800

    add firebase

--- gray/src/firebase/firebase.jsx
+++ gray/src/firebase/firebase.jsx
@@ -5,7 +5,6 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
