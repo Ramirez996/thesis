c2943070e6df6a73cbbd9904c68926bc6911b28e
Ramirez996 <ramirezjeffrey01996@gmail.com>
2025-10-11 08:07:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -406,8 +406,6 @@ def phq9_risk():
     features = np.array(answers)
     logit = np.dot(features, PHQ9_WEIGHTS) + PHQ9_INTERCEPT
     probability = 1/(1+np.exp(-logit))
-    risk_level = "High" if probability>=0.7 else "Low"
-
 
     anomaly_score = 0.0
     if text:
@@ -424,6 +422,7 @@ def phq9_risk():
             anomaly_score = 0.0
 
     hybrid_score = (probability+anomaly_score)/2
+    risk_level = "High" if hybrid_score >= 0.7 else "Low"
 
     cursor.execute("""
     INSERT INTO depression_results (
