77ca1792743685082b44f20598fafa4b83b23eff
Ramirez996 <ramirezjeffrey01996@gmail.com>
2025-10-23 17:54:05 +0800

    Change back

--- gray/src/supabaseClient.js
+++ gray/src/supabaseClient.js
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+// src/supabaseClient.js
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://lrsfyzndkmmtgrlnvwvy.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxyc2Z5em5ka21tdGdybG52d3Z5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwMzgxMjAsImV4cCI6MjA3NDYxNDEyMH0.ajJDHKrLHKupp6dWCI-JOyg3NOg2Fi3NeIXUinVdX6s';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
