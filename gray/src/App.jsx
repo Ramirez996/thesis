c2a75da80d5b7ef5f26b5c048f4e572275bf269b
Jeffrey Ramirez <ramirezjeffrey01996@gmail.com>
2025-10-30 20:44:45 +0800

    App update

--- gray/src/App.jsx
+++ gray/src/App.jsx
@@ -12,6 +12,7 @@ import WellTest from "./Testpages/WellTest";
 import Chatbot from "./pages/Chatbot";
 import "./Dashboard.css";
 import "./pages/anxiety.css";
+import AnxietyHistory from "./components/AnxietyHistory.jsx";
 
 const App = () => {
   return (
@@ -27,6 +28,7 @@ const App = () => {
                 <Route path="/chatbot" element={<Chatbot />} />
                 <Route path="/anxiety" element={<Anxiety />} />
                 <Route path="/anxiety-test" element={<AnxietyTest />} />
+                <Route path="/anxiety-history" element={<AnxietyHistory />} />
                 <Route path="/depression" element={<Depression />} />
                 <Route path="/depression-test" element={<DepressionTest />} />
                 <Route path="/well-being" element={<WellBeing />} />
