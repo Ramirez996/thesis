86c52615f5cf080a8759cc6a016f1fd2f52ce852
Jeffrey Ramirez <ramirezjeffrey01996@gmail.com>
2025-11-02 00:40:14 +0800

    Add Test History button to Wellbeing page

--- gray/src/pages/Wellbeing.jsx
+++ gray/src/pages/Wellbeing.jsx
@@ -33,6 +33,13 @@ const Wellbeing = () => {
         <button className="start-test-btn" onClick={() => navigate("/well-being-test")}>
           Start Test
         </button>
+        <br></br>
+        <button
+          className="history-btn"
+          onClick={() => navigate("/anxiety-history")}
+        >
+          Test History
+        </button>
       </div>
     </div>
   );
